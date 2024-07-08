647ed5a9bfaeedb4bdff70eb59702e5db177913d
Asimanye Dudumayo <adudumayo023@student.wethinkcode.co.za>
2024-07-09 00:12:40 +0200

    Progress: Add condtion for a 1x1 that does not have 1 as the element

--- matrixMaster.js
+++ matrixMaster.js
@@ -2,8 +2,18 @@ export function matrixOfMatrices(matrix) {
    if (Array.isArray(matrix)) {
       if (matrix.length === 0) {
          return false;
+      } else if (matrix.length === 1 && matrix[0] !== 1) {
+         return false;
       }
+
+      for (let i = 0; i < matrix.length; i++) {
+         var element = matrix[i];
+         console.log(element);
+      }
+
    } else {
       return false;
    }
 }
+
+matrixOfMatrices([[1, 2, 3], [3, 2, 1], [2, 1, 3]]);
