3e8160e16cd6a2593854d128ccbf4c72e18756b9
Asimanye Dudumayo <adudumayo023@student.wethinkcode.co.za>
2024-07-08 23:44:09 +0200

    Progress: Add the condition for an empty matrix

--- matrixMaster.js
+++ matrixMaster.js
@@ -1,3 +1,9 @@
 export function matrixOfMatrices(matrix) {
-    
+   if (Array.isArray(matrix)) {
+      if (matrix.length === 0) {
+         return false;
+      }
+   } else {
+      return false;
+   }
 }
