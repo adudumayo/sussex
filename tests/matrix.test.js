439bbd7956e45f644c4e4aa7c82275de544480fe
Asimanye Dudumayo <adudumayo023@student.wethinkcode.co.za>
2024-07-08 22:16:08 +0200

    Test case: Add test case for an expected false

--- tests/matrix.test.js
+++ tests/matrix.test.js
@@ -2,8 +2,13 @@ import { expect } from 'chai';
 import { matrixOfMatrix } from '../matrix.js';
 
 describe('matrixOfMatrix function', function() {
-    it('should take an n x n matrix and return true if all ints from 1 to n (incl) are in each matrice', function() {
+    it('should return true from a 3x3 matrix that has {1-3} in each and every matrix', function() {
         const result = matrixOfMatrix([[1, 2, 3], [3, 2, 1], [3, 1, 2]]);
         expect(result).to.equal(true);
+    });
+
+    it('should return false from a 3x3 matrix that does not have {1-3} in each and every matrix', function() {
+        const result = matrixOfMatrix([[1, 2, 3], [2, 3, 1], [3, 3, 1]]);
+        expect(result).to.equal(false);
     });
 });
